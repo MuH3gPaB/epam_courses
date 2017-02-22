c3ab6afa7130a206109696ca89e9621f6ec5048f
Mikhail <figurin_mih@mail.ru>
2017-02-22 15:18:20 +0300

    Unit02 folder added

---
+++ UNIT02/src/main/java/my/epam/stationery/Pen.java
@@ -0,0 +1,13 @@
+package my.epam.stationery;
+
+import java.awt.*;
+
+/**
+ * Created by M.Figurin on 22-Feb-17.
+ */
+public class Pen {
+    private Color brushColor;
+    private Color shellColor;
+    private String brandName;
+
+}
