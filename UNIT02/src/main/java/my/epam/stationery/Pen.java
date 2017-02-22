21d33b9949f6a26f38ab435063b52acd881d1b72
Mikhail <figurin_mih@mail.ru>
2017-02-22 17:41:16 +0300

    Simple pen class added

--- UNIT02/src/main/java/my/epam/stationery/Pen.java
+++ UNIT02/src/main/java/my/epam/stationery/Pen.java
@@ -6,8 +6,94 @@ import java.awt.*;
  * Created by M.Figurin on 22-Feb-17.
  */
 public class Pen {
-    private Color brushColor;
-    private Color shellColor;
-    private String brandName;
+    private final Color brushColor;
+    private final Color shellColor;
+    private final String brandName;
+    private final String type;
+    private final String label;
 
+    public static final String FOUNTAIN_PEN_TYPE = "FOUNTAIN";
+    public static final String BALLPOINT_PEN_TYPE = "BALLPOINT";
+    public static final String FIBER_TIP_PEN_TYPE = "FIBER_TIP";
+    public static final String ROLLER_BALL_PEN_TYPE = "ROLLER_BALL";
+    public static final String GEL_INK_PEN_TYPE = "GEL_INK";
+
+    public static final String DEFAULT_BRAND_NAME = "UNBRANDED";
+    public static final String DEFAULT_TYPE = "UNDEFINED";
+    public static final String DEFAULT_LABEL = "";
+
+    public static final Pen DEFAULT_BLACK_PEN = new Pen(Color.BLACK, Color.BLACK, DEFAULT_BRAND_NAME, DEFAULT_TYPE, DEFAULT_LABEL);
+    public static final Pen DEFAULT_BLUE_PEN = new Pen(Color.BLUE, Color.BLUE, DEFAULT_BRAND_NAME, DEFAULT_TYPE, DEFAULT_LABEL);
+    public static final Pen DEFAULT_RED_PEN = new Pen(Color.RED, Color.RED, DEFAULT_BRAND_NAME, DEFAULT_TYPE, DEFAULT_LABEL);
+
+
+    public Pen(Color brushColor, Color shellColor, String brandName, String type, String label) {
+        this.brushColor = brushColor;
+        this.shellColor = shellColor;
+        this.brandName = brandName;
+        this.type = type;
+        this.label = label;
+    }
+
+    public Pen(Color brushColor, Color shellColor) {
+        this(brushColor, shellColor, DEFAULT_BRAND_NAME, DEFAULT_TYPE, DEFAULT_LABEL);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null
+                || getClass() != o.getClass())
+            return false;
+
+        Pen pen = (Pen) o;
+
+        return brushColor.equals(pen.brushColor)
+                && shellColor.equals(pen.shellColor)
+                && brandName.equals(pen.brandName)
+                && type.equals(pen.type)
+                && label.equals(pen.label);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = brushColor.hashCode();
+        result = 31 * result + shellColor.hashCode();
+        result = 31 * result + brandName.hashCode();
+        result = 31 * result + type.hashCode();
+        result = 31 * result + label.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Pen{" +
+                "brushColor=" + brushColor +
+                ", shellColor=" + shellColor +
+                ", brandName='" + brandName + '\'' +
+                ", type='" + type + '\'' +
+                ", label='" + label + '\'' +
+                '}';
+    }
+
+    public Color getBrushColor() {
+        return brushColor;
+    }
+
+    public Color getShellColor() {
+        return shellColor;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
