8656ea96f6d4d6d7b9eb81e9dbe14fbd369c606a
Mikhail <figurin_mih@mail.ru>
2017-03-02 15:33:18 +0300

    StringBuilderStringStorage realization added. All storage tests are passed.

--- UNIT03/src/main/java/my/epam/unit03/task01/StringBuilderStringStorage.java
+++ UNIT03/src/main/java/my/epam/unit03/task01/StringBuilderStringStorage.java
@@ -1,35 +1,46 @@
 package my.epam.unit03.task01;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class StringBuilderStringStorage implements AbstractStringStorage{
+public class StringBuilderStringStorage implements AbstractStringStorage {
     private static final char SEPARATOR = 1;
     private final StringBuilder stringBuilder = new StringBuilder();
 
     @Override
     public void addString(String string) {
+        if (string.isEmpty()) throw new IllegalArgumentException("String should not be empty.");
         stringBuilder.append(string).append(SEPARATOR);
     }
 
     @Override
     public String[] findAll(String pattern) {
-        return (String[]) Arrays.stream(getAll())
+        if (pattern.isEmpty()) throw new IllegalArgumentException("String should not be empty.");
+        List<String> result = Arrays.stream(getAll())
                 .filter((str) -> str.contains(pattern))
-                .toArray();
+                .collect(Collectors.toList());
+        return result.toArray(new String[0]);
     }
 
     @Override
     public String findOne(String pattern) {
+        if (pattern.isEmpty()) throw new IllegalArgumentException("String should not be empty.");
+        for (String str : getAll()) {
+            if (str.contains(pattern)) return str;
+        }
         return null;
     }
 
     @Override
     public String[] getAll() {
-        return new String[0];
+        String data = stringBuilder.toString();
+        if(data.isEmpty()) return new String[0];
+        return data.split("" + SEPARATOR);
     }
 
     @Override
     public void clear() {
-
+        stringBuilder.setLength(0);
     }
 }
