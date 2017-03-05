0709dae4141ae4c0a745f93376df131f20468655
MuH3gPaB <figurin_mih@mail.ru>
2017-03-05 16:30:35 +0300

    Unit 03 - Task 03
    StringChecker javadoc added

--- UNIT03/src/main/java/my/epam/unit03/task03/StringChecker.java
+++ UNIT03/src/main/java/my/epam/unit03/task03/StringChecker.java
@@ -4,6 +4,21 @@ import java.util.Comparator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * String checker is string analytic class.
+ * <p>
+ * The goal is to analyse the order of some integer values in the string.
+ * Class use regexp pattern to find integer values.
+ * <p>
+ * Pattern could contain only one integer value mark, represented by
+ * "[0-9]{1,9}" sub-pattern.
+ * <p>
+ * If pattern is wrong illegal argument exception will thrown.
+ * <p>
+ * The order of integer values can be set by comparator given to constructor
+ * of StringChecker.
+ */
+
 public class StringChecker {
     private final Pattern pattern;
     private final static String patternChecker = "[0-9]{1,9}";
@@ -15,16 +30,20 @@ public class StringChecker {
         this.pattern = Pattern.compile(pattern);
     }
 
-    private static boolean checkPattern(String pattern) {
-        return pattern.contains(patternChecker)
-                && pattern.indexOf(patternChecker) == pattern.lastIndexOf(patternChecker);
-    }
-
+    /**
+     * Check if string is correct.
+     *
+     * @param minValue Minimum value that first founded integer could be.
+     * @param string   Checked string.
+     * @return New min value represented by maximum founded value, or old minValue if nothing was found
+     * @throws StringCheckFailException When the order of founded values is wrong, or first founded value less then minValue
+     */
     public int checkString(int minValue, String string) throws StringCheckFailException {
         Matcher matcher = pattern.matcher(string);
-        while (matcher.find()){
+        while (matcher.find()) {
             int founded = getValueFromString(matcher.group());
-            if (comparator.compare(founded, minValue) < 0) throw new StringCheckFailException("Wrong string '" + string + "'");
+            if (comparator.compare(founded, minValue) < 0)
+                throw new StringCheckFailException("Wrong string '" + string + "'");
             else minValue = founded;
         }
         return minValue;
@@ -38,6 +57,11 @@ public class StringChecker {
             throw new StringCheckFailException();
         }
 
+    }
+
+    private static boolean checkPattern(String pattern) {
+        return pattern.contains(patternChecker)
+                && pattern.indexOf(patternChecker) == pattern.lastIndexOf(patternChecker);
     }
 }
 
