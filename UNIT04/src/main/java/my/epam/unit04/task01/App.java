e2674823c711c18305a52784c6227dcd8469bd9a
Mikhail <figurin_mih@mail.ru>
2017-03-07 16:33:29 +0300

    Unit 04 - Task 01
    InputStream reading from file added.

--- UNIT04/src/main/java/my/epam/unit04/task01/App.java
+++ UNIT04/src/main/java/my/epam/unit04/task01/App.java
@@ -1,7 +1,9 @@
 package my.epam.unit04.task01;
 
 import my.epam.unit04.FileReader;
+import my.epam.unit04.StringWordsCounter;
 import org.apache.log4j.Logger;
+import sun.management.counter.StringCounter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,10 +14,19 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App {
     private static Logger logger = Logger.getLogger(App.class);
+
+    private static final String[] KEYWORDS_JAVA = new String[]{"abstract", "continue", "for", "new", "switch", "assert",
+            "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double",
+            "implements", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof",
+            "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void",
+            "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while"};
 
     public static void main(String[] args) throws URISyntaxException, FileNotFoundException {
         URL inputFileUrl = App.class.getResource("/my/epam/unit04/task01/CrazyLogger.javaText");
@@ -31,7 +42,12 @@ public class App {
     private static void calculateKeywordsToFile(InputStreamFileReader inputStreamFileReader, OutputStreamFileWriter outputStreamFileWriter) {
         String fileAsString = getJavaCodeAsString(inputStreamFileReader);
         String noComments = clearJavaComments(fileAsString);
-        System.out.println(noComments);
+        StringWordsCounter counter = new StringWordsCounter(KEYWORDS_JAVA);
+        Map<String, Integer> result = counter.calculateWords(noComments);
+        result = result.entrySet().stream()
+                .filter(e -> e.getValue() != 0)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        System.out.println(result.toString());
     }
 
     private static String clearJavaComments(String fileAsString) {
