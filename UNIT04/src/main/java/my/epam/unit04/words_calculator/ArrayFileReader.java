51df06ff34c1ba01c426cc4dbf6886362b4dc866
Mikhail <figurin_mih@mail.ru>
2017-03-10 20:07:22 +0300

    Optimized imports.

--- UNIT04/src/main/java/my/epam/unit04/words_calculator/ArrayFileReader.java
+++ UNIT04/src/main/java/my/epam/unit04/words_calculator/ArrayFileReader.java
@@ -1,7 +1,5 @@
 package my.epam.unit04.words_calculator;
 
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.stream.Stream;
 
 public interface ArrayFileReader {
--- UNIT04/src/main/java/my/epam/unit04/words_calculator/ArrayFileWriter.java
+++ UNIT04/src/main/java/my/epam/unit04/words_calculator/ArrayFileWriter.java
@@ -1,7 +1,5 @@
 package my.epam.unit04.words_calculator;
 
-import java.io.IOException;
-
 public interface ArrayFileWriter {
     void write(String[] strings);
 }
