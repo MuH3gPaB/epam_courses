a8860bf3f38caa00c91eb06129e4046171eee9ba
MuH3gPaB <figurin_mih@mail.ru>
2017-03-12 21:27:55 +0300

    Unit 05 - Task 02
    ResourceBundle implementation added

---
+++ UNIT05/src/main/java/my/epam/unit05/task02/PropertiesFileReader.java
@@ -0,0 +1,86 @@
+package my.epam.unit05.task02;
+
+import org.apache.log4j.Logger;
+import sun.util.ResourceBundleEnumeration;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.util.*;
+
+public class PropertiesFileReader extends ResourceBundle {
+    private static final PropertiesFileReader EMPTY_PROPERTIES_HOLDER = new PropertiesFileReader(new HashMap<>());
+
+    private static Logger logger = Logger.getLogger(PropertiesFileReader.class);
+
+    private Map<String, Object> properties;
+
+    private PropertiesFileReader(Map<String, Object> properties) {
+        this.properties = properties;
+    }
+
+
+    public static PropertiesFileReader getReader(File file) {
+        try {
+            Path path = file.toPath();
+            Map<String, Object> values = new HashMap<>();
+
+            Files.lines(path).forEach((s) -> {
+                parseStringToMap(values, s);
+            });
+
+            return new PropertiesFileReader(values);
+        } catch (IOException e) {
+            logger.error("Could not read the file [" + file.getName() + "]. " + e.getMessage());
+        }
+        return EMPTY_PROPERTIES_HOLDER;
+    }
+
+    @Override
+    protected Object handleGetObject(String key) {
+        if (properties.containsKey(key)) {
+            return properties.get(key);
+        } else {
+            logger.error("No such property [" + key + "]");
+            return null;
+        }
+    }
+
+    @Override
+    public Enumeration<String> getKeys() {
+        ResourceBundle parent = this.parent;
+        return new ResourceBundleEnumeration(properties.keySet(),
+                (parent != null) ? parent.getKeys() : null);
+    }
+
+    private static void parseStringToMap(Map<String, Object> values, String s) {
+        try {
+            String[] pair = checkPropertyLine(s);
+            Object value = parseValue(pair[1]);
+            values.put(pair[0], value);
+        } catch (IllegalArgumentException e) {
+            logger.warn("Parse error at line: [" + s + "]");
+        }
+    }
+
+    private static Object parseValue(String valueString) {
+        Object value;
+        if (valueString.matches("(^[0-9](1-9)[.][0-9](1-20)$)")) {
+            value = Double.parseDouble(valueString);
+        } else if (valueString.matches("(^[0-9](1-18)$)")) {
+            value = Integer.parseInt(valueString);
+        } else {
+            value = valueString;
+        }
+        return value;
+    }
+
+    private static String[] checkPropertyLine(String s) throws IllegalArgumentException {
+        String[] pair = s.split("=");
+        if (pair.length != 2) throw new IllegalArgumentException("Wrong line format. Should be one '=' symbol inside.");
+        return pair;
+    }
+
+}
