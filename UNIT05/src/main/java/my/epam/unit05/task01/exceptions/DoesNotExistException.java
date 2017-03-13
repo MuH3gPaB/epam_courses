d7c0461b722ecb7dbd1cdbd302bc19b50ce5124a
MuH3gPaB <figurin_mih@mail.ru>
2017-03-13 18:49:59 +0300

    Unit 05 - Task 01
    AlreadyExist and DoesNot exist exceptions added.

---
+++ UNIT05/src/main/java/my/epam/unit05/task01/exceptions/DoesNotExistException.java
@@ -0,0 +1,22 @@
+package my.epam.unit05.task01.exceptions;
+
+public class DoesNotExistException extends Exception{
+    public DoesNotExistException() {
+    }
+
+    public DoesNotExistException(String message) {
+        super(message);
+    }
+
+    public DoesNotExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DoesNotExistException(Throwable cause) {
+        super(cause);
+    }
+
+    public DoesNotExistException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
