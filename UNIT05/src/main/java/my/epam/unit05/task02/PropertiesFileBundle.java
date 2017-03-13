c1ff1d110e7371269095e4d6c559fa88f99d2965
MuH3gPaB <figurin_mih@mail.ru>
2017-03-13 13:13:19 +0300

    Unit 05 - Task 02
    Multiline for one property for array realization.

--- UNIT05/src/main/java/my/epam/unit05/task02/PropertiesFileBundle.java
+++ UNIT05/src/main/java/my/epam/unit05/task02/PropertiesFileBundle.java
@@ -1,11 +1,11 @@
 package my.epam.unit05.task02;
 
 import org.apache.log4j.Logger;
-import sun.reflect.Reflection;
 import sun.util.ResourceBundleEnumeration;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -32,9 +32,13 @@ public class PropertiesFileBundle extends ResourceBundle {
 
             return new PropertiesFileBundle(values);
         } catch (IOException e) {
-            logger.error("Could not read the file [" + file.getName() + "]. " + e.getMessage());
+            logger.warn("Could not read the file [" + file.getName() + "]. " + e.getMessage());
         }
         return EMPTY_PROPERTIES_HOLDER;
+    }
+
+    public int size() {
+        return properties.entrySet().size();
     }
 
     @Override
@@ -42,8 +46,17 @@ public class PropertiesFileBundle extends ResourceBundle {
         if (properties.containsKey(key)) {
             return properties.get(key);
         } else {
-            logger.error("No such property [" + key + "]");
-            return null;
+            logger.warn("No such property [" + key + "]");
+            if (isStringArrayRequired()) return new String[0];
+            else return "";
+        }
+    }
+
+    private boolean isStringArrayRequired() {
+        try {
+            throw new RuntimeException();
+        } catch (Exception e) {
+            return e.getStackTrace()[3].getMethodName().equals("getStringArray");
         }
     }
 
@@ -58,10 +71,38 @@ public class PropertiesFileBundle extends ResourceBundle {
         try {
             String[] pair = checkPropertyLine(s);
             Object value = parseValue(pair[1]);
-            values.put(pair[0], value);
+            if (values.containsKey(pair[0])) {
+                Object newValue = addElementTo(values.get(pair[0]), value);
+                values.put(pair[0], newValue);
+            } else {
+                values.put(pair[0], value);
+            }
         } catch (IllegalArgumentException e) {
-            logger.warn("Parse error at line: [" + s + "]");
+            logger.warn("Parse error at line: [" + s + "]" + e.getMessage());
         }
+    }
+
+    private static Object addElementTo(Object o, Object value) {
+        if (o.getClass().isArray()) {
+            checkClassesEquality(o.getClass().getComponentType(), value.getClass());
+            Object[] objArray = (Object[]) o;
+            Object[] newArray = (Object[]) Array.newInstance(value.getClass(), objArray.length + 1);
+            System.arraycopy(objArray, 0, newArray, 0, objArray.length);
+            newArray[objArray.length] = value;
+            return newArray;
+        } else {
+            checkClassesEquality(o.getClass(), value.getClass());
+            Object[] newArray = (Object[]) Array.newInstance(o.getClass(), 2);
+            newArray[0] = o;
+            newArray[1] = value;
+            return newArray;
+        }
+    }
+
+    private static void checkClassesEquality(Class o, Class value) {
+        if (!o.equals(value))
+            throw new IllegalArgumentException("Two properties with different types: " +
+                    "[" + o.getClass().getComponentType() + "] [" + value.getClass() + "]");
     }
 
     private static Object parseValue(String valueString) {
@@ -81,5 +122,4 @@ public class PropertiesFileBundle extends ResourceBundle {
         if (pair.length != 2) throw new IllegalArgumentException("Wrong line format. Should be one '=' symbol inside.");
         return pair;
     }
-
 }
--- UNIT05/src/test/java/my/epam/unit05/task02/PropertiesFileReaderTest.java
+++ UNIT05/src/test/java/my/epam/unit05/task02/PropertiesFileReaderTest.java
@@ -4,18 +4,58 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.PropertyResourceBundle;
 
 public class PropertiesFileReaderTest extends Assert {
 
     @Test
     public void existingFileWithValidProperties() {
-        File propFile = new File(getClass().getResource("/my/epam/unit05/task02/test-properties-01.properies").getFile());
+        File propFile = new File(getClass().getResource("/my/epam/unit05/task02/test-properties-01.properties").getFile());
         PropertiesFileBundle bundle = PropertiesFileBundle.getBundle(propFile);
 
         assertEquals("text", bundle.getString("text_propery"));
         assertEquals(10.12, bundle.getObject("double_property"));
         assertEquals(11, bundle.getObject("int_property"));
+    }
+
+    @Test
+    public void fileDoesNotExistShouldNotThrowException() {
+        try {
+            File propFile = new File("");
+            PropertiesFileBundle bundle = PropertiesFileBundle.getBundle(propFile);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void fileDoesNotExistShouldCreateEmptyBundle() {
+        File propFile = new File("");
+        PropertiesFileBundle bundle = PropertiesFileBundle.getBundle(propFile);
+        assertEquals(0, bundle.size());
+    }
+
+    @Test
+    public void getStringArrayTest() {
+        File propFile = new File("/my/epam/unit05/task02/test-properties-02.properties");
+        PropertiesFileBundle bundle = PropertiesFileBundle.getBundle(propFile);
+
+        String[] expected = {"element1", "element2"};
+        String[] actual = bundle.getStringArray("string_array");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void noSuchPropertyShouldNotThrowException() {
+        File propFile = new File("/my/epam/unit05/task02/test-properties-02.properties");
+        PropertiesFileBundle bundle = PropertiesFileBundle.getBundle(propFile);
+
+        try {
+            bundle.getString("");
+            bundle.getObject("");
+            bundle.getStringArray("");
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 
 }
