a7be4223be32009465f977237cc2cf43855cd607
Mikhail <figurin_mih@mail.ru>
2017-03-14 15:14:00 +0300

    Unit 05 - Task 01
    'append', 'read', 'help' commands added.

--- UNIT05/src/main/java/my/epam/unit05/task01/processor/ConsoleCommandProcessor.java
+++ UNIT05/src/main/java/my/epam/unit05/task01/processor/ConsoleCommandProcessor.java
@@ -4,29 +4,30 @@ import my.epam.unit05.task01.MainAppClass;
 import my.epam.unit05.task01.exceptions.AlreadyExistException;
 import my.epam.unit05.task01.exceptions.DoesNotExistException;
 import my.epam.unit05.task01.util.CustomExplorer;
+import my.epam.unit05.task01.util.TextFileAppender;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.Arrays;
 
 public class ConsoleCommandProcessor extends CommandProcessor {
     private CustomExplorer explorer;
     private final static String LINE_SEPARATOR = System.getProperty("line.separator");
 
     public enum Commands {
-        DIR("dir"),
-        CD("cd"),
-        EXIT("exit"),
-        MKDIR("mkdir"),
-        RMDIR("rmdir"),
-        MKFILE("mkfile"),
-        RMFILE("rmfile");
-
-        String value;
-
-        Commands(String value) {
-            this.value = value;
-        }
+        DIR,
+        CD,
+        EXIT,
+        MKDIR,
+        RMDIR,
+        MKFILE,
+        RMFILE,
+        APPEND,
+        READ,
+        HELP;
     }
 
     public ConsoleCommandProcessor(OutputStream out, CustomExplorer explorer) throws IOException {
@@ -37,14 +38,15 @@ public class ConsoleCommandProcessor extends CommandProcessor {
 
     @Override
     public void proceed(String commandString) throws IOException {
+        String[] commandStrings = commandString.trim().split(" ");
+        String commandName = commandStrings[0].toUpperCase();
         try {
-            String[] commandStrings = commandString.trim().split(" ");
-            Commands command = Commands.valueOf(Commands.class, commandStrings[0].toUpperCase());
+            Commands command = Commands.valueOf(Commands.class, commandName);
             String[] arguments = new String[commandStrings.length - 1];
             System.arraycopy(commandStrings, 1, arguments, 0, arguments.length);
             proceedCommand(command, arguments);
         } catch (IllegalArgumentException e) {
-            proceedUnknownCommand(commandString);
+            proceedUnknownCommand(commandName);
         }
     }
 
@@ -56,6 +58,15 @@ public class ConsoleCommandProcessor extends CommandProcessor {
 
     private void proceedCommand(Commands commands, String[] arguments) throws IOException {
         switch (commands) {
+            case HELP:
+                onHelp(arguments);
+                break;
+            case READ:
+                onRead(arguments);
+                break;
+            case APPEND:
+                onAppend(arguments);
+                break;
             case MKFILE:
                 onMkfile(arguments);
                 break;
@@ -76,6 +87,53 @@ public class ConsoleCommandProcessor extends CommandProcessor {
                 break;
             case EXIT:
                 onExit(arguments);
+        }
+    }
+
+    private void onHelp(String[] arguments) throws IOException {
+        String[] commands = new String[Commands.values().length];
+        commands[0] = "dir - list current folder";
+        commands[1] = "cd - change folder";
+        commands[2] = "exit - exit";
+        commands[3] = "mkdir - make new dir";
+        commands[4] = "rmdir - remove dir";
+        commands[5] = "mkfile - make new file";
+        commands[6] = "rmfile - remove file";
+        commands[7] = "append - append new text to existing file";
+        commands[8] = "read - read text file";
+        commands[9] = "help - this help";
+        printOut("Available commands:");
+        arrayPrintOut(commands);
+    }
+
+    protected void onRead(String[] arguments) throws IOException {
+        try {
+            File file = explorer.getFile(arguments[0]);
+            arrayPrintOut(Files.lines(file.toPath()).toArray(String[]::new));
+        } catch (DoesNotExistException e) {
+            printOut(e.getMessage());
+            printPreamble();
+        } catch (IOException e) {
+            printOut("Error reading the file.");
+            printPreamble();
+        }
+    }
+
+    protected void onAppend(String[] arguments) throws IOException {
+        try {
+            File file = explorer.getFile(arguments[0]);
+            TextFileAppender appender = TextFileAppender.getAppender(file);
+
+            StringBuilder sb = new StringBuilder();
+            Arrays.stream(arguments).skip(1).forEach((s) -> sb.append(s).append(" "));
+            appender.append(sb.toString());
+            printPreamble();
+        } catch (DoesNotExistException e) {
+            printOut(e.getMessage());
+            printPreamble();
+        } catch (IOException e) {
+            printOut("File write error.");
+            printPreamble();
         }
     }
 
--- UNIT05/src/main/java/my/epam/unit05/task01/util/TextFileAppender.java
+++ UNIT05/src/main/java/my/epam/unit05/task01/util/TextFileAppender.java
@@ -2,8 +2,10 @@ package my.epam.unit05.task01.util;
 
 import my.epam.unit05.task01.exceptions.DoesNotExistException;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 
@@ -14,14 +16,16 @@ public class TextFileAppender {
         this.file = file;
     }
 
-    public TextFileAppender getAppender(File file) throws DoesNotExistException, IOException {
-        if(!file.exists()) throw new DoesNotExistException("File ["+file.getName()+"] does not exist.");
-        if(!file.canWrite()) throw new IOException("File ["+file.getName()+"] is write protected.");
+    public static TextFileAppender getAppender(File file) throws DoesNotExistException, IOException {
+        if (!file.exists()) throw new DoesNotExistException("File [" + file.getName() + "] does not exist.");
+        if (!file.canWrite()) throw new IOException("File [" + file.getName() + "] is write protected.");
 
         return new TextFileAppender(file);
     }
 
     public void append(String text) throws IOException {
-        Files.write(file.toPath(), text.getBytes(), StandardOpenOption.APPEND);
+        Files.write(file.toPath(), text.getBytes(Charset.defaultCharset()), StandardOpenOption.APPEND);
+        Files.write(file.toPath(), System.getProperty("line.separator").getBytes(Charset.defaultCharset()), StandardOpenOption.APPEND);
+
     }
 }
